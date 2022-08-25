73011a34400ce565def8b40eff4a6f5af8e5092d
NdekoCode <arickbulakali@gmail.com>
2022-08-25 09:00:50 +0200

    add route on App.jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
 import About from "./pages/About";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <>
-      <Navigation />
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/scss/index.scss";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
