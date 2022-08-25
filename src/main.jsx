b70fb4203a5722f67485af5e1cf2963559f06d71
NdekoCode <arickbulakali@gmail.com>
2022-08-25 08:49:14 +0200

    styles: scss styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/scss/index.scss";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
