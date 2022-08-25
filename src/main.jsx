a42558a127e14eb1c2b5561403648f2c79e220f7
NdekoCode <arickbulakali@gmail.com>
2022-08-25 21:09:45 +0200

    remove scss data to skip conflict

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./assets/css/index.scss";
+import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
