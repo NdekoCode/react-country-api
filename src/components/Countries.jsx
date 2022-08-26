bf03ee609cf930ec358111649f2fde18230653a9
NdekoCode <arickbulakali@gmail.com>
2022-08-26 15:04:32 +0200

    feat: load and set data

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,8 +1,28 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Countries = () => {
+  const [state, setState] = useState({
+    data: [],
+    dataLoading: true,
+    dataError: "",
+  });
+
+  useEffect(() => {
+    try {
+      axios.get("https://restcountries.com/v3.1/lang/french").then((res) => {
+        if (res.status === 200) {
+          setState((d) => ({ ...d, data: res.data, dataLoading: false }));
+        } else {
+          setState((d) => ({ ...d, dataLoading: false }));
+        }
+      });
+    } catch (error) {
+      setState((d) => ({ ...d, dataError: false }));
+    }
+  }, []);
+  console.log(state);
   return (
-    <div>
+    <div className="countries">
       <h1>Countries</h1>
     </div>
   );
